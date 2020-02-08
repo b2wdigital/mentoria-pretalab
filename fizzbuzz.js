614c550b5fe9ba27550e0f96899158ddee00a021
Hillary de Sousa Ribeiro <hillary.ribeiro@b2wdigital.com>
2020-02-08 13:50:53 -0300

    mentoria

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,13 @@
 function fizzbuzz(i) {
-    return;
+    if (i%3==0 && i%5==0) {
+        return 'FizzBuzz';
+    } else if (i%3==0) {
+        return 'Fizz';
+    }
+    else if (i%5==0){
+        return 'Buzz';
+    }
+    return i;
 }
 
 module.exports = fizzbuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,14 @@
 const fizzbuzz = require('./fizzbuzz');
 
 test('return fizz if number is divisible by 3', () => {
-  expect(fizzbuzz(3)).toBe("fizz");
+  expect(fizzbuzz(3)).toBe("Fizz");
+});
+test('return buzz if number is divisible by 5', () => {
+  expect(fizzbuzz(10)).toBe("Buzz");
+});
+test('return fizzBuzz if number is divisible by 3 and 5', () => {
+  expect(fizzbuzz(15)).toBe("FizzBuzz");
+});
+test('returne o número se não for divisível por 3 e 5', () => {
+  expect(fizzbuzz(2)).toBe(2);
 });
